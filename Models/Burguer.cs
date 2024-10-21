a9b9ce066a0be755d31379fb6a83dca9020d6e0d
Bastian2704 <sebastianabad2704@gmail.com>
2024-10-21 10:32:16 -0500

    Creacion Clase

---
+++ Models/Burguer.cs
@@ -0,0 +1,11 @@
+﻿namespace SAbadBurguerPromoCF.Models
+{
+    public class Burguer
+    {
+        public int BurguerId { get; set; }
+        public string Nombre { get; set; }
+        public bool WithCheese { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
