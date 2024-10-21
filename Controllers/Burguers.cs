07ab54da60b5b861c8501fd38dc58c8ca05c33d6
laboratorio <laboratorio@UPEAULA-31714>
2024-10-21 10:13:19 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/Burguers.cs
@@ -0,0 +1,10 @@
+﻿namespace SAbadBurguerPromoCF.Controllers
+{
+    public class Burguers
+    {
+        public int BurguerID { get; set; }
+        public string Nombre { get; set; }
+        public bool WithCheese { get; set; }
+        public double price { get; set; }
+    }
+}
